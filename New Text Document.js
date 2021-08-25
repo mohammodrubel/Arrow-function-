7c2ff5c0191a6836a54449184c5d48ec5bb20e68
Mohammodrubel <74967048+mohammodrubel@users.noreply.github.com>
2021-08-25 18:30:45 +0600

    Add files via upload

---
+++ New Text Document.js
@@ -0,0 +1,35 @@
+// example one 
+const myArrow=(x)=>{
+    console.log(x)
+}
+myArrow('hello world')
+
+// example two 
+const myFunc = (...myArray)=>{
+    console.log(myArray)
+}
+myFunc(1,2,3,4,5,6,7,8,9)
+const functionReturn = ()=>{}
+
+// example Three 
+const myFunction = (number)=> number *5 
+console.log(myFunction(5))
+
+// example Fore 
+const myF = ()=> console.log('hello world')
+myF()
+
+// example Five
+const myArroFunction = ()=> 'little programmer'
+console.log(myArroFunction())
+// example six 
+const mynewFunction= (num1,num2)=>{
+    const num3 = num1 + num2
+    return num3
+}
+mynewFunction(40,50)
+console.log(mynewFunction(40,50))
+
+//example seven
+const myNewFunc = (num1,num2)=> num1+ num2
+console.log(myNewFunc(49,56))
